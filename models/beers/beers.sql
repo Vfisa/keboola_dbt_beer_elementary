b87dddbe67d0284cb67ef0d722c16d0749295e0b
VFisa <vfisa.stuff@gmail.com>
2022-08-21 15:25:58 -0700

    init

---
+++ models/beers/beers.sql
@@ -0,0 +1,19 @@
+
+{{ config(
+    materialized="table"
+) }}
+
+SELECT
+  "id"            AS beer_id,
+  TRIM("name")    AS beer_name,
+  "style"         AS beer_style,
+  TRY_TO_NUMERIC("abv",14,8)            AS abv,
+  TRY_TO_NUMERIC("ibu",14,8)            AS ibu,
+  CASE 
+       WHEN ibu <= 50 THEN 'Malty'
+       WHEN ibu > 50 THEN 'Hoppy'
+   END AS bitterness,
+  "brewery_id"    AS brewery_id,
+  "ounces"        AS ounces
+FROM
+  {{ source('in.c-dbt_beer_seed', 'seed_beers') }}
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Test project
+
+ - implementing beer demo by datafold
+ - including elementary package
+ - including keboola target runner
+
+## Run
+
+`dbt debug -t kbc_dev_dbt_beer  --profiles-dir .`
